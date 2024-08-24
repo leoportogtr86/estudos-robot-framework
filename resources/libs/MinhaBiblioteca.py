a6b3e7100a10bb63169cd50fde2332aff055655f
Leo Porto <leonardoreisporto@gmail.com>
2024-08-24 08:21:14 -0300

    exercicios

--- resources/libs/MinhaBiblioteca.py
+++ resources/libs/MinhaBiblioteca.py
@@ -1,2 +1,13 @@
 def soma_dois_numeros(a, b):
+    """
+    Esta função soma dois números inteiros.
+
+    Parâmetros:
+    a (int): Primeiro números a ser somado.
+    b (int): Segundo números a ser somado.
+
+    Retorna:
+    int: Resultado da soma
+    """
+    # Corpo da função
     return int(a) + int(b) 
